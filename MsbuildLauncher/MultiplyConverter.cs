f5bd44803a35feaf57cedde00a3dc2474c8e48a5
Yoichi Imai <sunnyone41@gmail.com>
2013-02-19 01:10:07 +0900

    Set property font size relatively small.

---
+++ MsbuildLauncher/MultiplyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MsbuildLauncher
+{
+    class MultiplyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double d1 = (double)value;
+            double d2 = Double.Parse((string) parameter);
+
+            return d1 * d2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
